e6108948ff731f945e5b09ca34c44b548143232d
dawkaka <yousiph77@gmail.com>
2023-02-05 12:13:19 +0000

    feat: minimal add questions

--- pages/api/questions.ts
+++ pages/api/questions.ts
@@ -6,10 +6,8 @@ import { authOptions } from "./auth/[...nextauth]"
 
 export default async function questionsHandler(req: NextApiRequest, res: NextApiResponse) {
     const session = await getServerSession(req, res, authOptions)
-    console.log(session)
-    const { query, method } = req
-    const id = parseInt(query.id as string, 10)
-    const name = query.name as string
+
+    const { method } = req
 
     if (!session) {
         return res.status(401).json({ message: "Login required" })
@@ -19,12 +17,12 @@ export default async function questionsHandler(req: NextApiRequest, res: NextApi
         case 'GET':
             // Get data from your database
             try {
-                const userQuestions = await prisma.user.findUnique({ where: { email: session.user?.image || undefined } })
+                const userQuestions = await prisma.user.findUnique({ where: { email: session.user?.email || undefined } })
                 if (!userQuestions) {
                     return res.status(404).json({ message: "user not found" })
                 }
 
-                res.status(200).json(userQuestions)
+                res.status(200).json(userQuestions.questions)
 
             } catch (error) {
                 return res.status(400).json({ error: error })
@@ -32,17 +30,31 @@ export default async function questionsHandler(req: NextApiRequest, res: NextApi
             break
         case 'PUT':
             try {
-                const userQuestions = await prisma.user.update({ where: { email: session.user?.image || undefined }, data: { questions: req.body } })
-                if (!userQuestions) {
-                    return res.status(404).json({ message: "user not found" })
+                // Validate the req.body
+                if (!Array.isArray(req.body)) {
+                    return res.status(400).json({ message: "The request body must be an array of questions" });
                 }
-
-                res.status(200).json(userQuestions)
-
-            } catch (error) {
-                return res.status(400).json({ error: error })
+                if (req.body.length > 25) {
+                    return res.status(400).json({ message: "The number of questions in the array cannot be more than 25" });
+                }
+                for (let i = 0; i < req.body.length; i++) {
+                    const question = req.body[i];
+                    if (!question.question || typeof question.question !== "string" || question.question.length > 256) {
+                        return res.status(400).json({ message: "Each question must be a string not greater than 256 characters" });
+                    }
+                    if (!question.type || (question.type !== "openEnded" && question.type !== "closeEnded" && question.type !== "both")) {
+                        return res.status(400).json({ message: "Each question must have a type of either 'openEnded', 'closeEnded', or 'both'" });
+                    }
+                }
+                const userQuestions = await prisma.user.update({
+                    where: { email: session.user?.email || undefined },
+                    data: { questions: req.body },
+                });
+                res.status(200).json(userQuestions);
+            } catch (error: any) {
+                return res.status(400).json({ error: error.message });
             }
-            break
+            break;
         default:
             res.setHeader('Allow', ['GET', 'PUT'])
             res.status(405).end(`Method ${method} Not Allowed`)
