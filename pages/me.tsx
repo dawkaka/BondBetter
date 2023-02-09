126458cb8254fae06dbb182b791858bd4dfd463b
dawkaka <yousiph77@gmail.com>
2023-02-09 09:28:23 +0000

    make it responsive and shit

--- pages/me.tsx
+++ pages/me.tsx
@@ -6,11 +6,11 @@ export default function MePage() {
   return (
     <Layout>
       <div className="w-full flex flex-col items-center mt-[60px]">
-        <div className="flex flex-col h-[700px] w-[min(100%,700px)]">
+        <div className="flex flex-col w-[min(100%,700px)] px-3">
           <div className="flex flex-col items-center w-full py-16 border-b">
-            <div className="flex items-center gap-10">
+            <div className="flex flex-col sm:flex-row items-center gap-10">
               <img src={data?.user?.image!} alt="" className="rounded-full" />
-              <div>
+              <div className="flex flex-col items-center">
                 <p className="font-bold text-2xl text-center">{data?.user?.name}</p>
                 <p>{data?.user?.email}</p>
               </div>
