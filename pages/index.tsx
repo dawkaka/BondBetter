259e8ce2d613e802912931525c861525e6284ad5
dawkaka <yousiph77@gmail.com>
2023-02-14 15:54:36 +0000

    mer cur ind

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,10 +44,14 @@ export default function IndexPage() {
 
 const Quiz = ({ questions }: { questions: { question: string }[] }) => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [answers, setAnswers] = useState<any>({});
+  const [answers, setAnswers] = useState<{ id: number, answer: string }[]>(new Array(questions.length).fill("").map((_, ind) => {
+    return { id: ind, answer: "--No Answer--" }
+  }));
 
   const handleAnswerChange = (e: any) => {
-    setAnswers({ ...answers, [currentQuestionIndex]: e.target.value });
+    const curr = answers
+    curr[currentQuestionIndex].answer = e.target.value
+    setAnswers([...curr]);
   };
 
   const handlePrevButtonClick = () => {
@@ -59,18 +63,31 @@ const Quiz = ({ questions }: { questions: { question: string }[] }) => {
   };
 
   return (
-    <div className="w-full h-full flex flex-col items-center bg-purple-700">
+    <div className="w-full h-full flex flex-col items-center bg-purple-700 overflow-auto">
       <Container>
-        <div className="w-[min(100%,550px)] my-auto flex flex-col items-center">
-          <div className="w-full flex rounded">
-            <div className="w-full rounded p-4 sm:p-10 flex flex-col gap-10">
-              <h3 className="text-2xl text-white font-bold">
+        <div className="w-[min(100%,550px)] flex flex-col items-center">
+          <div className="w-full mt-[10vh] p-4 sm:p-10 flex justify-between items-center">
+            <div className="flex gap-1">
+              {
+                answers.map(a => {
+                  return (
+                    <div className={`w-3 h-3 rounded-full ${a.answer === "--No Answer--" || a.answer === "" ? "bg-red-500" : "bg-green-500"}`}></div>
+                  )
+                })
+              }
+            </div>
+            <p className="text-white rounded-full text-sm px-2 py-1 font-bold bg-purple-500"> {currentQuestionIndex + 1} / 5</p>
+          </div>
+          <div className="w-full flex rounded min-h-[50vh]">
+            <div className="w-full rounded my-auto p-4 sm:p-10 flex flex-col gap-10">
+              <h3 className="text-2xl text-white font-semibold">
                 {questions[currentQuestionIndex].question}
               </h3>
               <textarea
+                autoFocus
                 rows={1}
                 className="bg-transparent border-b px-3 py-2 focus:outline-none text-white text-lg"
-                value={answers[currentQuestionIndex] || ""}
+                value={answers[currentQuestionIndex].answer !== "--No Answer--" ? answers[currentQuestionIndex].answer : ""}
                 onChange={handleAnswerChange}
               >
               </textarea>
