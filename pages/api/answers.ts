fa6c6462a62de0efbfc861f1592e80c34777980f
dawkaka <yousiph77@gmail.com>
2023-02-19 18:54:08 +0000

    feat: add answered daily questions

--- pages/api/answers.ts
+++ pages/api/answers.ts
@@ -13,15 +13,45 @@ export default async function requestHandler(req: NextApiRequest, res: NextApiRe
     }
     switch (method) {
         case "GET":
-            const user = await prisma.user.findUnique({ where: { email: session.user.email! }, select: { coupleID: true } })
-            if (!user) {
+            const user = await prisma.user.findUnique({ where: { email: session.user.email! }, select: { coupleID: true, id: true } })
+            if (user === null) {
                 return res.status(404).json({ message: "Something went wrong" })
             }
             if (!user.coupleID) {
                 return res.json({ type: 'No Partner', message: 'Not a couple' })
             }
-            const answers = await prisma.coupleAnswer.findMany({ where: { coupleID: user.coupleID } })
-            res.json(answers)
+            const rawAns = await prisma.coupleAnswer.findMany({
+                where: { coupleID: user.coupleID },
+                select: { user: { select: { name: true } }, question: { select: { question: true } }, answer: true, time: true, questionID: true, userID: true }
+            })
+            const answers = rawAns.filter(ans => ans.user !== null)
+
+            const ans = answers.reduce((acc, val) => {
+                if (!val.user) return acc
+                if (acc[val.questionID]) {
+                    let v = acc[val.questionID]
+                    if (!v) return acc
+                    if (val.userID === user.id) {
+                        v.user = { name: val.user.name, answer: val.answer }
+                    } else {
+                        v.partner = { name: val.user.name, answer: val.answer }
+                    }
+                } else {
+                    let v: { user?: {}, partner?: {}, question: string } = { question: val.question.question }
+                    if (val.userID === user.id) {
+                        v.user = { name: val.user.name, answer: val.answer }
+                    } else {
+                        v.partner = { name: val.user.name, answer: val.answer }
+                    }
+                    acc[val.questionID] = v
+                }
+                return acc
+            }, <{ [key: number]: { user?: {}, partner?: {}, question: string } }>{})
+
+            let filtered = Object.values(ans).filter(v => {
+                return v.user !== undefined
+            })
+            res.json({ filtered, count: filtered.length })
             break;
         default:
             break;
